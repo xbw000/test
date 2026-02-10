d6fcd8b40bbc705b9f609f2d67b4aa3c080668a7
“xue-bowenbb” <13152805913@163.com>
2026-02-10 17:23:27 +0800

    ss

---
+++ project/main.c
@@ -0,0 +1,23 @@
+#include "CH58x_common.h"
+#include "msh_log.h"  
+#include "drv_gpio.h"
+#define LED0_PIN    drv_gpio_get(B,17)
+
+int main()
+{
+    uint8_t len;
+
+    HSECFG_Capacitance(HSECap_18p);
+    SetSysClock(CLK_SOURCE_HSE_PLL_78MHz);
+
+    Msh_Init(); 
+    LOG_I("System Power On...");
+    drv_gpio_mode_cfg(LED0_PIN,GPIO_ModeOut_PP_20mA);
+    drv_gpio_write(LED0_PIN,PIN_HIGH);
+    drv_gpio_write(LED0_PIN,PIN_HIGH);
+    drv_gpio_write(LED0_PIN,PIN_HIGH);
+    while(1)
+    {
+        Msh_Process();
+    }
+}
